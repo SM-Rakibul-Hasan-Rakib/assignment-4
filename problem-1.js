1d6b52d020b0fd3144dc308b0862683f3b5bc85a
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-07 22:17:55 +0600

    DF

--- problem-1.js
+++ problem-1.js
@@ -2,5 +2,13 @@
 function calculateMoney(ticketSale) {
 
   // You have to write your code here
-  if()
+  
+  const ticketSalePrice = 120;
+  const oneSecurityDailySallery = 500;
+  const stafNumber = 8;
+  const stafLunchBill = 50;
+
+  const 
   }
+  const total = ticketSale.amount(10);
+  console.log(total);
